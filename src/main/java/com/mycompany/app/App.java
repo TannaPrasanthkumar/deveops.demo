e01b01a71327a3d54c841aabc97b45f444c87e10
TannaPrasanthkumar <115206363+TannaPrasanthkumar@users.noreply.github.com>
2023-08-31 15:24:27 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,28 @@
 package com.mycompany.app;
 
+import java.lang.*;
+import java.util.*;
+
 public class App
 {
+    public void Alpha_To_Numerical(char ch)
+    {
+        String str="abcdefghijklmnopqrstuvwxyz";
+        for(int i=0;i<str.length();i++)
+        {
+            if(str.charAt(i)==ch)
+            {
+                System.err.println(i+1);
+                break;
+            }
+        }
+    }
     public static void main(String[] args)
     {
-         System.out.println("This is the Prasanth kumar tanna");
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+
+        App a1 = new App();
+        a1.Alpha_To_Numerical(ch);
     }
 }
